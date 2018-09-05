61243689e0f11b11000c9f09cf9891e25d831164
Simon Baerlocher <s.baerlocher@sbaerlocher.ch>
2018-09-05 15:14:40 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Chocolatey: Crealogix PayMaker Office
+
+## Description
+
+The multi-banking software PayMaker Office offers you comprehensive features for your payment transactions, with an overview and transparency at all times and across different banks. You can make payments of all types and in a wide variety of formats quickly and reliably.
+
+## Package Parameters
+
+* `/CleanStartmenu` Removes frequently used Crealogix PayMaker Office shortcuts from the Startmenu.
+* `/RemoveDesktopIcons` Removes the desktop icon from Crealogix PayMaker Office.
+
+## Installation
+
+installation without parameters.
+
+```ps1
+choco install crealogix-paymaker-office
+```
+
+installation with parameters.
+
+```powershell
+ choco install crealogix-paymaker-office --params="'/RemoveDesktopIcons /CleanStartmenu'"
+```
+
+## Disclaimer
+
+These Chocolatey Packages only contain installation routines. The software itself is downloaded from the official sources of the software developer. ITIGO AG has no affilation with the software developer.
+
+## Author
+
+* [Simon Bärlocher](https://sbaerlocher.ch)
+* [ITIGO AG](https://www.itigo.ch)
+
+## License
+
+This project is under the MIT License. See the [LICENSE](LICENSE) file for the full license text.
+
+## Copyright
+
+(c) 2018, ITIGO AG
---
+++ crealogix-paymaker-office/tools/chocolateyInstall.ps1
@@ -0,0 +1,46 @@
+﻿# crealogix-paymaker-office install
+
+$ErrorActionPreference = 'Stop';
+
+$toolsDir            = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$PackageParameters   = Get-PackageParameters
+$urlZip              = 'https://shop.crealogix.com/download/get/file/pm5.office'
+$checksumZip         = '7da52b4a856fd1f23f50947480917d4c4e3a468f27034716b3b60572ba6d92b8d5ec4707def427855e9ecadf07d4e93d1edd432cba166f535a1dd69219e01c94'
+$checksumTypeZip     = 'SHA512'
+$unzipLocation       = "$($env:TMP)\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageVersion)"
+$version             = "$($env:ChocolateyPackageVersion.split(".")[0, 1, 2] -join '.')"
+
+Import-Module -Name "$($toolsDir)\helpers.ps1"
+
+$zipArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    url            = $urlZip
+    unzipLocation  = $unzipLocation
+    checksum       = $checksumZip
+    checksumType   = $checksumTypeZip
+}
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    fileType       = 'MSI'
+    file           = "$($unzipLocation)\PayMaker$($version).msi"
+    silentArgs     = $silentArgs
+    ValidExitCodes = @(0, 1000, 1101)
+}
+
+Install-ChocolateyZipPackage @zipArgs
+
+Install-ChocolateyInstallPackage @packageArgs
+
+if ($PackageParameters.RemoveDesktopIcons) {
+    Remove-DesktopIcons -Name "PayMaker" -Desktop "Public"
+}
+
+if ($PackageParameters.CleanStartmenu) {
+    Remove-FileItem `
+        -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\PayMaker"
+    Install-ChocolateyShortcut `
+        -ShortcutFilePath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\PayMaker.lnk" `
+        -TargetPath "C:\Program Files (x86)\CLX.PayMaker\PaymentStudio.exe" `
+        -WorkDirectory "C:\Program Files (x86)\CLX.PayMaker\"
+}
